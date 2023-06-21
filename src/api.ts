c55fe3ca60b7d23c6f28c723f0a43d65a4660a1d
engulfedInFlames <recona97@gmail.com>
2023-06-21 16:47:33 +0900

    update: .env.template 수정

--- src/api.ts
+++ src/api.ts
@@ -9,7 +9,7 @@ import {
 } from "./type";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true,
 });
 
